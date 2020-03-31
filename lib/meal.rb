3aa7022a3297a1316ac32e038dc932288c22f0a4
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-31 20:06:16 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,19 +1,18 @@
 class Meal
-  
-  attr_accessor :waiter, :customer, :total, :tip 
-  
-  @@all= []
-  
-  def initalize(waiter, customer, total, tip = 0)
+ 
+  attr_accessor :waiter, :customer, :total, :tip
+ 
+  @@all = []
+ 
+  def initialize(waiter, customer, total, tip=0)
     @waiter = waiter
     @customer = customer
     @total = total
-    @tip = tip 
+    @tip = tip
     @@all << self
-  end 
-  
-  def self.all 
+  end
+ 
+  def self.all
     @@all
-  end 
-
+  end
 end
